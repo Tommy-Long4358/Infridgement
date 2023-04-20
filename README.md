b178d7ad7ef9b4efdfc873abf3793206ba935794
Matt-Hoang <61717506+Matt-Hoang@users.noreply.github.com>
2023-04-20 01:46:46 -0700

    home, ratio, and nav bar ui

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Infridgement
-https://tommy-long4358.github.io/Infridgement
+Website: https://tommy-long4358.github.io/Infridgement
+Figma: https://www.figma.com/file/0KZWWraor1zXYaJp6tiveH/Infridgement-Prototype?node-id=0-1&t=UYwYAwXPl2kcUgEs-0
 
+tasks: 
+- login, registration, confirmation --> Matt (Done)
+- item list (home left side) --> Tommy
+- fridge capacity (home right side) --> Matt
+- custom item (add left side) --> Chris
+- preset item (add - right side) --> son
+- ratios --> Matt
+- notifications (profile left) --> Zack 
+- personal info (profile right) --> Mariel 
+- about page --> Mariel 
+- set section sizes in db --> Matt (should be easy) 
--- javascript/firebaseKey.js
+++ javascript/firebaseKey.js
@@ -1,22 +1,11 @@
 // Firebase Key
 
-// qqgteam491@gmail.com
-// export const firebaseConfig = {
-//     apiKey: "AIzaSyCzpEwrHIAT1MfPfW9RXMxR0wNOFpcnS-Y",
-//     authDomain: "qqg-491b.firebaseapp.com",
-//     databaseURL: "https://qqg-491b-default-rtdb.firebaseio.com",
-//     projectId: "qqg-491b",
-//     storageBucket: "qqg-491b.appspot.com",
-//     messagingSenderId: "891674221190",
-//     appId: "1:891674221190:web:a4bcf43906da1a6dd3cf07"
-// };
-
-// matt's email (for dev)
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCQwT0cYqdKAPVFm3b_QRNVGmzxIGuaDNc",
-    authDomain: "infridgement-75efb.firebaseapp.com",
-    projectId: "infridgement-75efb",
-    storageBucket: "infridgement-75efb.appspot.com",
-    messagingSenderId: "392779302296",
-    appId: "1:392779302296:web:2f7cef7b0d88aa131f97ed"
-  };
+  apiKey: "AIzaSyDd8OVuXXSDNoFyE7R0DXA_hgo5L1bK3wc",
+  authDomain: "infridgement-de310.firebaseapp.com",
+  projectId: "infridgement-de310",
+  storageBucket: "infridgement-de310.appspot.com",
+  messagingSenderId: "1007141359831",
+  appId: "1:1007141359831:web:3b36334a2088170991e092"
+};
